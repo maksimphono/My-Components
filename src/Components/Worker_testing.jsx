b791a798efd5f50c205ee4eeaa96e734f1d39ea2
Maxun <trukhinmaksimof@mail.ru>
2022-12-01 21:57:25 +0800

    added and described useWorker hook

--- src/Components/Worker_testing.jsx
+++ src/Components/Worker_testing.jsx
@@ -1,18 +1,38 @@
-import WorkerBuilder from "./worker-builder.js";
-import FiboWorker from "./fibo.worker.js";
-import {useEffect} from "react";
+/*
+    Instruction : 
 
-const workerInstance = new WorkerBuilder(FiboWorker);
+    Install package:
+    npm install --force @koale/useworker
 
-export default function () {
-    useEffect(() => {
-        workerInstance.onmessage = (message) => {
-            console.log("Message from worker ", message.data);
-        }
-    }, []);
+    useWorker(fn, option)   hook takes in 
+        fn (expensive function, that must be executed)
+        options (object with values : 
+            timeout - time to stop worker execution
+            remoteDependencies - array of strings, contains the remote dependencies needed to run the worker
+            autoTerminate - bool kill the worker once it's done
+        )
+    and returns Array of 2 values : expensive function promice and controller:
+        controller is an Object of 2 keys: status (status of resurned callback) and kill (callback to terminate worker)
+*/
+
+import {useEffect, useMemo, useCallback} from "react";
+import {useWorker} from "@koale/useworker";
+
+export default function Test_useWorker(props){
+    const arr = useMemo(() => [...Array(9999999)].map(e => Math.random() * 1000000), []);
+    const sortFn = useCallback(array => array.sort(), []);
+
+    const [workerFn] = useWorker(sortFn);
+
+    const runSort = async () => {
+        const result = await workerFn(arr);
+        alert("Worker end up sort with result : ");
+        console.table(result.slice(0, 6));
+    };
+
     return (
-        <button onClick = {() => workerInstance.postMessage(799999999)}>
-            Send Message
+        <button onClick = {runSort}>
+            Sort
         </button>
-    )
+    );
 }
